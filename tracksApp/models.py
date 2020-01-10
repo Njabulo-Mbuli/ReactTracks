9eb4b31966ea652e2d2bb9061084610c787fb4cf
Njabulo-Mbuli <admin@njabulombuli.co.zagit config --global user.email admin@njabulombuli.co.za>
2020-01-10 19:59:24 +0200

    created the models

--- tracksApp/models.py
+++ tracksApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+class Track(models.Model):
+    #id is added automatically
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(get_user_model(),null=True,on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(get_user_model(),null=True,on_delete=models.CASCADE)
+    track = models.ForeignKey('tracksApp.Track',related_name="likes",on_delete=models.CASCADE)
